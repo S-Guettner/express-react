6669cfa37f41df7bb67e70679a58a322c87e5073
S-Guettner-Dev <sven_guettner@protonmail.com>
2023-03-27 15:12:17 +0200

    frontend

--- server/app.js
+++ server/app.js
@@ -1,6 +1,38 @@
 import  express  from 'express'
+import fs from 'fs'
 import cors from 'cors'
 
 const app = express()
 const PORT = 8787
 
+
+
+
+app.use(cors({ origin: "http://localhost:5173/message/" }))
+
+
+app.post("/", (req, res) => {
+    const data = req.body
+    console.log(data)
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen(PORT, () => console.log("Server läuft auf PORT:" + PORT))
